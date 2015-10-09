e3bd560e33b084145f24870905ebc2acb8a077e5
Clement Auger <clement.ager@d1m.cn>
2015-10-09 15:26:32 +0800

    add new nodeAction debug_with to use HTML comments instead of fake HTML dom node. It s very useful for invisible nodes like <html>

--- src/C/ModernApp/File/Helpers/DashboardHelper.php
+++ src/C/ModernApp/File/Helpers/DashboardHelper.php
@@ -1,6 +1,7 @@
 <?php
 namespace C\ModernApp\File\Helpers;
 
+use C\Layout\Transforms;
 use C\ModernApp\File\AbstractStaticLayoutHelper;
 use C\ModernApp\Dashboard\Transforms as Dashboard;
 use C\ModernApp\File\FileTransformsInterface;
@@ -24,6 +25,17 @@ class DashboardHelper extends  AbstractStaticLayoutHelper{
                     ->setLayout($T->getLayout())
                     ->setExtensions($this->extensions)
                     ->show(__CLASS__, $nodeContents);
+            }
+            return true;
+        }
+    }
+
+    public function executeBlockNode (FileTransformsInterface $T, $subject, $nodeAction, $nodeContents) {
+        if ($nodeAction==="debug_with") {
+            if ($T->getLayout()->debugEnabled) {
+                Transforms::transform()
+                    ->setLayout($T->getLayout())
+                    ->setDefaultMeta($subject, ['debug_with' => $nodeContents]);
             }
             return true;
         }
