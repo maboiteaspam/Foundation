abf477da2e26f3bb9527e23822062857d5657308
Clement Auger <clement.ager@d1m.cn>
2015-10-19 18:06:19 +0800

    doc comments

--- src/C/View/Context.php
+++ src/C/View/Context.php
@@ -2,11 +2,25 @@
 namespace C\View;
 
 use C\Layout\Block;
+use C\Misc\ArrayHelpers;
 use C\View\Helper\ViewHelperInterface;
 
+/**
+ * Class Context
+ * Is the context within which the view is rendered.
+ * It s the $this you ll use within a view file.
+ *
+ * It has helpers, and a magic method __call
+ * to help your developers to access helpers facilities more easily.
+ *
+ * @package C\View
+ */
 class Context {
 
-    public $helpers = [];
+    /**
+     * @var ArrayHelpers
+     */
+    public $helpers;
 
     /**
      * @var Block
@@ -14,16 +28,30 @@ class Context {
     public $block;
 
     public function __construct () {
+        $this->helpers = new ArrayHelpers();
     }
 
+    /**
+     * @param ViewHelperInterface $helper
+     * @deprecated use $helpers directly
+     */
     public function addHelper (ViewHelperInterface $helper) {
-        $this->helpers[] = $helper;
+        $this->helpers->append($helper);
     }
 
+    /**
+     * @param ViewHelperInterface $helper
+     * @deprecated use $helpers directly
+     */
     public function prependHelper (ViewHelperInterface $helper) {
-        array_unshift($this->helpers, $helper);
+        $this->helpers->prepend($helper);
     }
 
+    /**
+     * Set the next block top render on each helpers.
+     *
+     * @param Block $block
+     */
     public function setBlockToRender (Block $block) {
         $this->block = $block;
         foreach($this->helpers as $helper) {
@@ -32,6 +60,15 @@ class Context {
         }
     }
 
+    /**
+     * Call a method on helpers.
+     * Stops after first valid helper found.
+     *
+     * @param $method
+     * @param $args
+     * @return mixed
+     * @throws \Exception
+     */
     public function __call($method, $args){
         foreach($this->helpers as $helper) {
             if (method_exists($helper, $method)) {
