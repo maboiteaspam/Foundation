f8bca20aa0f2f1ff9e7fe9fa42cf7e8333177a0e
Clement Auger <clement.ager@d1m.cn>
2015-10-19 18:07:00 +0800

    doc comments

--- src/C/Layout/TransformsInterface.php
+++ src/C/Layout/TransformsInterface.php
@@ -1,11 +1,32 @@
 <?php
 namespace C\Layout;
 
+/**
+ * Interface TransformsInterface
+ *
+ * Transforms are responsible to apply transformation on layout and their block.
+ *
+ * @package C\Layout
+ */
 interface TransformsInterface{
 
+    /**
+     * @param Layout $layout
+     * @return mixed
+     */
     public function setLayout (Layout $layout);
 
+    /**
+     * @return Layout
+     */
     public function getLayout ();
 
+    /**
+     * @param $id
+     * @param array $data
+     * @return mixed
+     */
+    public function setDefaultData($id, $data=[]);
 
+    // @todo put more definition of Transform methods here
 }
