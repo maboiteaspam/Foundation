4293aae7cf22c2adc72581596569bf277cea80d2
Clement Auger <clement.ager@d1m.cn>
2015-10-21 10:42:58 +0800

    Add capability to set a default route to submit forms created via layouts

--- src/C/ModernApp/File/Helpers/FormViewHelper.php
+++ src/C/ModernApp/File/Helpers/FormViewHelper.php
@@ -2,10 +2,11 @@
 namespace C\ModernApp\File\Helpers;
 
 use C\Form\FormBuilder;
-use C\Layout\Transforms;
 use C\ModernApp\File\AbstractStaticLayoutHelper;
 use C\ModernApp\File\FileTransformsInterface;
 use Symfony\Component\Form\FormFactory;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Validator\Constraints\Length;
 
 /**
  * Class FormViewHelper
@@ -27,6 +28,59 @@ class FormViewHelper extends  AbstractStaticLayoutHelper{
     }
 
     /**
+     * The route name to submit forms.
+     *
+     * @var string
+     */
+    protected $defaultRoute = null;
+    /**
+     * The method type for form submission
+     * @var string
+     */
+    protected $defaultMethod = null;
+    /**
+     * An array of extra parameters injected
+     * in the url generator.
+     *
+     * @var array
+     */
+    protected $defaultRouteParameters = null;
+
+    /**
+     * Set a default route to submit form.
+     *
+     * @param $name
+     */
+    public function setSubmitRoute($name) {
+        $this->defaultRoute = $name;
+    }
+
+    /**
+     * Add extra route parameters.
+     *
+     * @param array $parameters
+     */
+    public function setRouteParameters(array $parameters) {
+        $this->defaultRouteParameters = $parameters;
+    }
+
+    /**
+     * Set a default method for each created form.
+     * @param $method
+     */
+    public function setDefaultMethod ($method) {
+        $this->defaultMethod = $method;
+    }
+
+    /**
+     * @var UrlGenerator
+     */
+    protected $urlGenerator;
+    public function setUrlGenerator ( UrlGenerator $g) {
+        $this->urlGenerator = $g;
+    }
+
+    /**
      * Looks for 'create_form' node actions,
      * create a form object and populate it with provided fields,
      * inject the form into the default view data.
@@ -40,21 +94,29 @@ class FormViewHelper extends  AbstractStaticLayoutHelper{
     public function executeBlockNode (FileTransformsInterface $T, $blockSubject, $nodeAction, $nodeContents) {
         if ($nodeAction==="create_form") {
 
-            $form           = $this->formFactory->createBuilder()->getForm();
             $formId         = isset($nodeContents['name']) ? $nodeContents['name'] : 'form';
             $formContent    = isset($nodeContents['children']) ? $nodeContents['children'] : [];
+
+            $builder = $this->formFactory->createBuilder();
+
+            if ($this->defaultMethod) $builder->setMethod($this->defaultMethod);
+            if ($this->defaultRoute) $builder->setAction(
+                $this->urlGenerator->generate($this->defaultRoute, array_merge([],$this->defaultRouteParameters,[
+                    "block"     => $blockSubject,
+                    "formId"    => $formId,
+                ]))
+            );
 
             foreach ($formContent as $elName=>$elData) {
                 $type = isset($elData['type']) ? $elData['type'] : 'text';
                 $options = isset($elData['options']) ? $elData['options'] : [];
                 $attr = isset($elData['attr']) ? $elData['attr'] : [];
                 $options['attr'] = isset($options['attr']) ? array_merge($attr, $options['attr']) : $attr;
-                $form->add($elName, $type, $options);
+                $builder->add($elName, $type, $options);
             }
 
-            Transforms::transform()
-                ->setLayout($T->getLayout())
-                ->setDefaultData($blockSubject, [$formId=>FormBuilder::createView($form)]);
+            $T->setDefaultData($blockSubject, [$formId=>FormBuilder::createView($builder->getForm())]);
+
             return true;
         }
     }
