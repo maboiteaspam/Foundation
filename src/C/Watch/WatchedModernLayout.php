d2d54f8cb35730bc1ee5289956fb96121e07ba45
Clement Auger <clement.ager@d1m.cn>
2015-10-21 15:05:37 +0800

    bugfix, sometimes watcher lost track of some files, it needs a little help to make it friendly to use again

--- src/C/Watch/WatchedModernLayout.php
+++ src/C/Watch/WatchedModernLayout.php
@@ -52,6 +52,12 @@ class WatchedModernLayout extends WatchedRegistry {
             if ($item) {
                 $this->store->storeFile($item['absolute_path']);
                 $updated = true;
+            } else if ($this->registry->isInRegisteredPaths($file)) {
+                $this->registry->addItem($file);
+                $this->registry->saveToCache();
+                $item = $this->registry->get($file);
+                $this->store->storeFile($item['absolute_path']);
+                $updated = true;
             }
         }
         return $updated;
