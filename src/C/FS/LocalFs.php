062bee91e0bfdb0742c4acd69664200ad9a4f5eb
Clement Auger <clement.ager@d1m.cn>
2015-10-21 16:44:31 +0800

    doc and code review

--- src/C/FS/LocalFs.php
+++ src/C/FS/LocalFs.php
@@ -1,16 +1,35 @@
 <?php
 
 namespace C\FS;
+
 use C\Misc\Utils;
 
+/**
+ * Class LocalFs
+ * is a facade to php FS functions.
+ * It  s main purpose is to record calls and there stack trace.
+ *
+ * For convenience some methods are explictly delcared,
+ * otherwise it relies on the magic calls.
+ *
+ * @package C\FS
+ */
 class LocalFs {
 
     public static $record = false;
     public static $allcalls = [];
-    public $calls = [];
-    public function __construct( ) {
-    }
 
+    /**
+     * @var array
+     */
+    public $calls = [];
+
+    /**
+     * Records and saves the stack trace of a call made via magic methods.
+     * Useful to know where file system calls occurs.
+     * @param $method
+     * @param $args
+     */
     public function recordCall ($method, $args) {
         if (self::$record) {
             $stack = Utils::getStackTrace();
@@ -19,17 +38,40 @@ class LocalFs {
         }
     }
 
+    /**
+     * Magic method to call original
+     * php function as static method
+     * of this class.
+     *
+     * @param $method
+     * @param $args
+     * @return mixed
+     */
     public function __call ($method, $args) {
         $this->recordCall($method, $args);
         return call_user_func_array($method, $args);
     }
 
+    /**
+     * Records and saves the stack trace of ALL calls including magic methods.
+     *
+     * @param $method
+     * @param $args
+     * @param null $stack
+     */
     public static function recordAllCalls($method, $args, $stack=null) {
         if (self::$record) {
             self::$allcalls[] = [$method, $args, $stack?$stack:Utils::getStackTrace()];
         }
     }
 
+    /**
+     * Let you call php FS functions as static method of this class.
+     *
+     * @param $method
+     * @param $args
+     * @return mixed
+     */
     public static function __callStatic($method, $args) {
         self::recordAllCalls($method, $args);
         return call_user_func_array($method, $args);
