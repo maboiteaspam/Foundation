18e605d8a8df3009b86896ef818817c8f11d870c
clement <maboiteaspam@wanadoo.fr>
2015-10-22 21:28:56 +0800

    doc

--- src/C/Form/FormErrorHelper.php
+++ src/C/Form/FormErrorHelper.php
@@ -4,10 +4,43 @@ namespace C\Form;
 use \Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormError;
 
+/**
+ * Class FormErrorHelper
+ * helps to transform a submitted form object
+ * into a useful serializable array of elements => error messages
+ *
+ * @see https://github.com/symfony/symfony/issues/7205
+ * @package C\Form
+ */
 class FormErrorHelper
 {
     /**
-     * @see https://github.com/symfony/symfony/issues/7205
+     * Take a form in input and return an error array.
+     * it puts global errors into a special globals__ key,
+     *      see SF\Form error_bubbling option.
+     *
+     * For each element of the form with a constraint,
+     * it determine a path such
+     * [formName]_[elementName]=>[errors]
+     *      form_name=>[errors]
+     *      form_address=>[errors]
+     *      form42_mobile=>[errors]
+     *      ect
+     * for array values such names[],
+     * the array will contain a sub array for each sub element,
+     * and so on.
+     * Such as acceding the first element is
+     *      [formName]_[elementName][0]=>[errors]
+     *      form42_mobiles[0]=>Invalid mobile phone !
+     *
+     * [
+     *  globals__=>[ msg1, msg2 ],
+     *  elementPath1=>[ msg1, msg2 ],
+     *  elementPath2=>[
+     *      subElementIndex0=>[ msg1, msg2 ],
+     *      subElementIndex1=>[ msg1, msg2 ],
+     *  ],
+     * ];
      *
      * @param Form $form
      * @return array Associative array of all errors
@@ -36,7 +69,7 @@ class FormErrorHelper
         return $errors;
     }
 
-    protected function getFormChildErrors ($form, $name="") {
+    protected function getFormChildErrors ($form) {
         $errors = [];
 
         if ($form instanceof Form) {
@@ -48,7 +81,7 @@ class FormErrorHelper
                 /** @var $child Form */
                 $err = $this->getFormChildErrors($child);
                 if (count($err)>0) {
-                    $errors[$name.$child->getPropertyPath()->__toString()] = $err;
+                    $errors[$child->getPropertyPath()->__toString()] = $err;
                 }
             }
         }
