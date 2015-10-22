5ea63a9d72efb063b5e2c1efb2ab5316de091902
Clement Auger <clement.ager@d1m.cn>
2015-10-22 14:23:44 +0800

    move those resources import to the new form service provider

--- src/C/Provider/IntlServiceProvider.php
+++ src/C/Provider/IntlServiceProvider.php
@@ -89,21 +89,6 @@ class IntlServiceProvider implements ServiceProviderInterface
      **/
     public function boot(Application $app)
     {
-        $fs = $app['intl.fs'];
-        /* @var $fs KnownFS */
-        if (isset($app['validator'])) {
-            $r = new \ReflectionClass('Symfony\Component\Validator\Validation');
-            $path = dirname($r->getFilename()).'/Resources/translations/';
-            $fs->register($path, 'Validator');
-        }
-
-        if (isset($app['form.factory'])) {
-            $r = new \ReflectionClass('Symfony\Component\Form\Form');
-            $path = dirname($r->getFilename()).'/Resources/translations/';
-            $fs->register($path, 'Validator');
-        }
-
-
         if (isset($app['watchers.watched'])) {
             $app['watchers.watched'] = $app->share($app->extend('watchers.watched', function($watched, Application $app) {
                 $w = new WatchedIntl();
