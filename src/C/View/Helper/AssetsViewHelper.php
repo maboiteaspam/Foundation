f98e8249c57cfcde51772e3b62ef36cf69060196
Clement Auger <clement.ager@d1m.cn>
2015-10-22 14:21:13 +0800

    doc

--- src/C/View/Helper/AssetsViewHelper.php
+++ src/C/View/Helper/AssetsViewHelper.php
@@ -13,6 +13,9 @@ class AssetsViewHelper implements ViewHelperInterface {
     public $block;
     public $currentInline;
 
+    /**
+     * @param Block $block
+     */
     public function setBlockToRender ( Block $block) {
         $this->block = $block;
 //        if ($this->currentInline!==null) echo 'bad';
@@ -20,16 +23,28 @@ class AssetsViewHelper implements ViewHelperInterface {
     }
 
     public $assetPatterns = [];
-
+    /**
+     * @param $patterns
+     */
     public function setPatterns ($patterns) {
         $this->assetPatterns = $patterns;
     }
-
+    /**
+     * @param $pattern
+     */
     public function addPattern ($pattern) {
         $this->assetPatterns[] = $pattern;
     }
 
 
+    /**
+     * Get url of an asset given it s name and its parameters.
+     *
+     * @param $name
+     * @param array $options
+     * @param array $only
+     * @return mixed|string
+     */
     public function urlAsset ($name, $options=[], $only=[]) {
         $url = '';
         $imgUrls = $this->assetPatterns;
@@ -47,14 +62,23 @@ class AssetsViewHelper implements ViewHelperInterface {
     /**
      * starts recording of a script/css inline.
      * $target is first head foot last
+     *
+     * Recorded block will be displayed as-is in the defined $target block.
+     *
      * @param $target
+     * @throws \Exception
      */
     public function inlineTo ($target) {
         $this->currentInline = $target;
+        if (!in_array($target, ['first','head','foot','last',]))
+            throw new \Exception('target must be one of first / head / foot / last');
 //        if ($this->currentInline!==null) echo 'bad';
         ob_start();
     }
 
+    /**
+     * End recording of an asset block.
+     */
     public function endInline() {
 //        if ($this->currentInline===null) echo 'bad';
         $content = ob_get_clean();
@@ -63,6 +87,12 @@ class AssetsViewHelper implements ViewHelperInterface {
         $this->currentInline = null;
     }
 
+    /**
+     * Let you inject an asset from the view.
+     * @param $target
+     * @param $asset
+     * @param bool $first
+     */
     public function useAsset($target, $asset, $first=false) {
         $this->block->addAssets([$target=>[$asset]], $first);
     }
