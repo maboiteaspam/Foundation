f5c57c74823fef522ce99bcfd579b907c3732d43
clement <maboiteaspam@wanadoo.fr>
2015-10-27 21:05:29 +0800

    Book update

--- src/C/ModernApp/DashboardExtension/templates/options.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* @var $this \C\View\ConcreteContext */
-?>
-<b class="dashboard-title">Options</b>
-
-<div class="dashboard-block-content">
-    TBD
-</div>
--- src/C/ModernApp/DashboardExtension/templates/time-travel.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* @var $this \C\View\ConcreteContext */
-?>
-<b class="dashboard-title">Time travel</b>
-
-<div class="dashboard-block-content">
-    TBD
-</div>
