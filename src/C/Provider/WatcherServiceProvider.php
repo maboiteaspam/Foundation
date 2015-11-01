c5a7594a43e8d8cbcee23a0c7a313e7e6f6d3fae
clement <maboiteaspam@wanadoo.fr>
2015-11-01 13:21:14 +0800

    (service) added comments

--- src/C/Provider/WatcherServiceProvider.php
+++ src/C/Provider/WatcherServiceProvider.php
@@ -4,24 +4,21 @@ namespace C\Provider;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
+/**
+ * Class WatcherServiceProvider
+ * provide ability to connect your objects
+ * to the watcher system provided by c2-bin
+ *
+ * @package C\Provider
+ */
 class WatcherServiceProvider implements ServiceProviderInterface
 {
-    /**
-     *
-     * @param Application $app
-     **/
     public function register(Application $app)
     {
         $app['watchers.watched'] = $app->share(function() {
             return [];
         });
     }
-    /**
-     *
-     * @param Application $app Silex application instance.
-     *
-     * @return void
-     **/
     public function boot(Application $app)
     {
     }
