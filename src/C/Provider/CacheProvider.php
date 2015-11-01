464adbf8df9ba6f6f3e8bcecf30f6eb1f7ce2c84
clement <maboiteaspam@wanadoo.fr>
2015-11-01 13:09:53 +0800

    (service) add new cache factory cache.get, added comments cache.get helps to reduce code duplication by encapsulating the knowledge required to select a cache a store

--- src/C/Provider/CacheProvider.php
+++ src/C/Provider/CacheProvider.php
@@ -4,12 +4,26 @@ namespace C\Provider;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
+/**
+ * Class CacheProvider
+ *
+ * extends the original cache provider
+ * to register a new cache drivers IncludeCache.
+ *
+ * IncludeCache is cool t use because the content
+ * is easier to read than serialize, the original serializer.
+ *
+ * Although, it require to use a dirty hack
+ * which consist of a str_replace of stdClass::__set_state to ''
+ * so, it s not recommended to use it in production.
+ *
+ *
+ * @package C\Provider
+ */
 class CacheProvider implements ServiceProviderInterface
 {
-    /**
-     *
-     * @param Application $app
-     **/
+    // extends cache.drivers service,
+    // register the new cache driver include.
     public function register(Application $app)
     {
         if (isset($app['cache.drivers'])) {
@@ -18,14 +32,18 @@ class CacheProvider implements ServiceProviderInterface
                 return $drivers;
             });
         }
+
+        // provides a cache factory given a store name
+        // it switches to default cache if the given store
+        // is not configured
+        $app['cache.get'] = $app->protect(function ($storeName) use ($app) {
+            if (isset($app['caches'][$storeName]))
+                $cache = $app['caches'][$storeName];
+            else
+                $cache = $app['cache'];
+
+            return $cache;
+        });
     }
-    /**
-     *
-     * @param Application $app Silex application instance.
-     *
-     * @return void
-     **/
-    public function boot(Application $app)
-    {
-    }
+    public function boot(Application $app) {}
 }
