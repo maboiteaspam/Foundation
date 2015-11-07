6f7bd738ab122b2971b2b0717129fb8e40f57473
clement <maboiteaspam@wanadoo.fr>
2015-11-07 21:52:09 +0800

    (fixture) add support for fixture generation

---
+++ src/C/Fixture/Generator.php
@@ -0,0 +1,41 @@
+<?php
+namespace C\Fixture;
+
+use C\Stream\StreamConcat;
+use C\Stream\StreamFlow;
+
+/**
+ * Class Generator
+ * helps to generate entities
+ * to use in fixtures
+ *
+ * @package C\Fixture
+ */
+class Generator{
+
+    /**
+     * push $len times
+     * a clone of $what
+     * modified with $transform
+     * returns the resulting array $results of data
+     *
+     * @param $what
+     * @param $transform
+     * @param int $len
+     * @return \ArrayObject
+     */
+    public static function generate ($what, $transform, $len=10) {
+        $results = new \ArrayObject();
+        $concat = new StreamConcat();
+        StreamFlow::duplex($len)
+            ->pipe($transform)
+            ->pipe($concat->appendTo($results))
+            ->write($what);
+        return $results;
+    }
+
+}
+
+// see
+// https://github.com/maboiteaspam/StreamObjectTransform
+// https://github.com/maboiteaspam/BlogData
